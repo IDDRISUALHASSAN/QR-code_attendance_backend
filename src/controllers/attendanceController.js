403fa8f65d4d7c8e53f039d245baa5c8b7450fdc
Your Name <user@example.com>
2026-09-12 14:14:39 +0000

    location extend

--- src/controllers/attendanceController.js
+++ src/controllers/attendanceController.js
@@ -1,6 +1,7 @@
 import Attendance from "../models/Attendance.js";
 import AttendanceSession from "../models/AttendanceSession.js";
 import User from "../models/User.js";
+allow
 
 /*
 |--------------------------------------------------------------------------
@@ -168,7 +169,7 @@ export const scanAttendance = async (req, res) => {
     // 8. Attendance distance rule
     // --------------------------------------------------
 
-    const ALLOWED_RADIUS = 200000;
+    const ALLOWED_RADIUS = 300000;
 
     if (calculatedDistance > ALLOWED_RADIUS) {
       return res.status(403).json({
